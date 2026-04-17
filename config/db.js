e90f69717ac93ab4071d9cd918e8aca53de4fa2a
babader08 <babamboup697@gmail.com>
2026-04-17 22:07:43 +0000

    🚀 Fix: connexion base de données dynamique

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const { Pool } = require("pg");
+require("dotenv").config(); // Très important pour lire le .env
 
 const pool = new Pool({
-  user: "projetStage01",
-  host: "127.0.0.1",
-  database: "postgres",
-  password: "babader",
-  port: 5432,
+  // Si DATABASE_URL existe (sur Render), on l'utilise.
+  // Sinon, on utilise tes paramètres locaux du .env
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect((err, client, release) => {
   if (err) {
     return console.error("Erreur de connexion à Postgres", err.stack);
   }
-  console.log("Connecté à la base de données PostgreSQL !");
+  console.log("✅ Connecté à la base de données PostgreSQL !");
   release();
 });
 
